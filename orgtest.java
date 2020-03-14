b8929092fb6a8928c56ae0ba9953faf511bda609
tqy <2978806030@qq.com>
2020-03-14 15:44:34 +0800

    姓名测试

--- orgtest.java
+++ orgtest.java
@@ -4,6 +4,7 @@ public class orgtest {
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
 		System.out.println("唐巧云-610");
+                                System.out.println("唐巧云");
  
 	}
 
